bee72d633b08ae505040ae3c219af87f01d6af8e
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-12 23:26:07 +0300

    portfolio lang C#

---
+++ chet_ne_chet.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("<четное или нечетное>");
+Console.Write("введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = a % 2;
+if (result > 0) Console.Write("нечетное");
+if (result <= 0) Console.Write("четное"); //?
