0956ecf548066242df40edffea943d5b296e1a7c
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-16 01:18:49 +0300

    portfolio lang C#

---
+++ zadacha2.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.");
+
+void input() {
+    Console.WriteLine("введите два числа");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    //Console.Write("{0}, {1}", number1, number2);
+    if (number2 % number1 == 0) {
+        Console.WriteLine("второе кратно первому");
+    } else {
+        Console.WriteLine("второе не кратно первому");
+    }
+}
+
+input();
