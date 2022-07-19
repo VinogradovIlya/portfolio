9fef62dfaaa710f552887c0353843bc18e38a59f
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-19 20:15:55 +0300

    portfolio lang C#

---
+++ nature_M_N.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N..");
+Console.Write("введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Nature(m, n);
+
+void Nature(int m, int n) {
+    // if (chislo < M || chislo > N) {
+    if (m < n) {
+        Nature(m, n - 1);
+    }
+    Console.Write("{0} ", n);
+ }
