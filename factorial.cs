f3f73387ae81454fd9b76a5cafe0324a35d6031b
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-22 19:10:03 +0300

    portfolio

---
+++ factorial.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задайте значение N. Напишите программу, которая найдет факториал числа N рекурсивным методом.");
+
+Console.Write("введите N: ");
+int vvod = Convert.ToInt32(Console.ReadLine());
+Factorial(vvod);
+Console.Write(Factorial(vvod));
+
+
+int Factorial(int vvod) {
+    if (vvod > 0) {
+        //Factorial(vvod - 1) * vvod;
+        return Factorial(vvod - 1) * vvod;
+    }
+    return 1;
+}
+// почему при вводе Н более 32 вылазит абра кадабра?(
---
+++ na_proverky/factorial.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задайте значение N. Напишите программу, которая найдет факториал числа N рекурсивным методом.");
+
+Console.Write("введите N: ");
+int vvod = Convert.ToInt32(Console.ReadLine());
+Factorial(vvod);
+Console.Write(Factorial(vvod));
+
+
+int Factorial(int vvod) {
+    if (vvod > 0) {
+        return vvod * Factorial(vvod - 1);
+    } 
+    return 1;
+}
+// а как же принципы структурного программирования?
+
+// с 13 не верно
+// почему при вводе Н более 32 вылазит абра кадабра?(
