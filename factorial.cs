bfa891f239b8ea0305b58952c55e11c085347264
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-22 20:42:23 +0300

    portfolio lang C#

--- factorial.cs
+++ factorial.cs
@@ -8,9 +8,7 @@ Console.Write(Factorial(vvod));
 
 int Factorial(int vvod) {
     if (vvod > 0) {
-        //Factorial(vvod - 1) * vvod;
-        return Factorial(vvod - 1) * vvod;
-    }
+        return vvod * Factorial(vvod - 1);
+    } 
     return 1;
 }
-// почему при вводе Н более 32 вылазит абра кадабра?(
