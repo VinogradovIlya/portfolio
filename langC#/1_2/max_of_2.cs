4e60025df10ef2abdc1e0fd640694b99bead1690
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-24 20:40:21 +0300

    portfolio

---
+++ langC#/1_2/max_of_2.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("<максимум из двух чисел>");
+Console.Write("введите 1 число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите 2 число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("наибольшее число -- ");
+if (a > b) Console.WriteLine("{0}", a);
+if (a < b) Console.WriteLine("{0}", b);
