11d041260cc8473310d2871ef88812c0037650db
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-24 21:26:50 +0300

    portfolio

---
+++ langC#/10_0/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Написать программу, принимающую на вход массив строк и генерирующую новый массив, состоящий из элементов, начинающихся на символ s1 и заканчивающихся на символ s2");
+
+Console.Write("введите размер массива: ");
+string[] CreateStringArray( number);
+
+string[] CreateStringArray(int number) {
+    string[] pupkaAlupka = new string[number];
+    for (int i = 0; i < number; i++) {
+        Console.Write($"введите {i+1} слово: ");
+        pupkaAlupka[i] = Console.ReadLine();
+    }
+    return pupkaAlupka;
+}
+
+
+// int main() {
+//     if () {
+//         return a
+//     }
+//     return b
+// }
