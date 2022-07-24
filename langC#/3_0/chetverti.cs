b61a7e54ef6ed290f7c07fe5b2dc7e9c4480ff0c
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-24 20:50:38 +0300

    portfolio sem3

---
+++ langC#/3_0/chetverti.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).");
+
+Console.Write("введите четверть: ");
+int vvod = Convert.ToInt32(Console.ReadLine());
+Chetverti(vvod);
+
+int Chetverti(int vvod) {
+    if (vvod > 0 || vvod < 4) {
+        if (vvod == 1) Console.Write("x > 0 и y > 0");
+        if (vvod == 2) Console.Write("x < 0 и y > 0");
+        if (vvod == 3) Console.Write("x < 0 и y < 0");
+        if (vvod == 4) Console.Write("x > 0 и y < 0");
+    }
+    return -1;
+}
