4f14e1667921062caf9031ff4fe5ef59a12ecff8
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-07-28 20:36:05 +0300

    portfolio

---
+++ C#/4/4_27/summa_cifr_v_chisle.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+
+Console.Write("введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
