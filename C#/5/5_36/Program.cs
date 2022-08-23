0f3c43154bccf3f7f7c00fcdef6045e5f563940c
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-08-23 21:24:38 +0300

    portfolio lang C#

---
+++ C#/5/5_36/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Задайте массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+
+int[] array = new int[10];
+
+/* два метода: 
+первый создает массив*/
+MethodArrayOne(array);
+ShowArray(array);
+Console.WriteLine();
+// второй: считает сумму
+// MethodArrayTwo(array);
+// Console.WriteLine("{0}", MethodArrayTwo(array));
+
+int[] MethodArrayOne(int[] array) {
+    for (int i = 0; i < 10; i++) {
+        array[i] = new Random().Next(-999, 1000);
+    }
+    return array;
+}
+
+void ShowArray(int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write(array[i] + " ");
+    }
+}
+
+// void MethodArrayTwo(int[] array) {
+
+// }
