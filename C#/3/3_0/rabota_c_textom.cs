05dad2eb20fbdbcb77ae33b2aa005ecbe949e544
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-08-31 16:17:48 +0300

    portfolio lang C#

--- C#/3/3_0/rabota_c_textom.cs
+++ C#/3/3_0/rabota_c_textom.cs
@@ -11,7 +11,7 @@ string Replace(string text, char oldValue, char newValue) {
     string result = String.Empty;
     int length = text.Length;
     for (int i = 0; i < length; i++) {
-        if(text[i] == oldValue) result = result + $"{newValue}";
+        if (text[i] == oldValue) result = result + $"{newValue}";
         else result = result + $"{text[i]}";
     }
     return result;
---
+++ C#/final/final.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("введите данные: ");
+string text = Console.ReadLine();
+
+
+string StringSize(string text) {
+    
+    int count = 0;
+    
+    for (int i = 0; i < text.Length; i++) {
+        if (text[i] == " ") count = 0;
+        else {
+            int newCount = 0;
+            newCount++;
+            string rezult = string.Empty;
+            if (newCount <= 3) result = result + $"{text[i]}";
+        }
+    }
+    return result;
+}
