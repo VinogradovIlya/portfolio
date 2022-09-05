b8e35902c2cb7041a481c49096a3e11b5c7a397a
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-09-05 19:55:09 +0300

    portfolio lang Python

--- na_proverky/dz_01.py
+++ na_proverky/dz_01.py
@@ -1,18 +1,21 @@
 print('Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.')
 
-day = input('введите день недели: ')
-if day == '1':
-    print('нет')
-elif day == '2':
-    print('нет')
-elif day == '3':
-    print('нет')
-elif day == '4':
-    print('нет')
-elif day == '5':
-    print('нет')
-elif day == '6':
-    print('да')
-elif day == '7':
-    print('да')
-else: print('пробуй еще')
+day = int(input('введите день недели: '))
+# if day == '1':
+#     print('нет')
+# elif day == '2':
+#     print('нет')
+# elif day == '3':
+#     print('нет')
+# elif day == '4':
+#     print('нет')
+# elif day == '5':
+#     print('нет')
+# elif day == '6':
+#     print('да')
+# elif day == '7':
+#     print('да')
+# else: print('пробуй еще')
+
+if 5 < day <= 7: print('да')
+else: print('нет')
