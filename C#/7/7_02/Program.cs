83c315229c1751625c2479d10157b23cce52bee7
Илья Старцев <ilastarcev@UmpaBook-Air.local>
2022-09-07 08:45:06 +0300

    portfolio lang C#

--- C#/7/7_02/Program.cs
+++ C#/7/7_02/Program.cs
@@ -1,5 +1,18 @@
 ﻿Console.WriteLine("Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.");
 
+Console.Write("введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Show2dArray(
+    CreateRandom2dArray(m, n, -10, 10));
+Console.WriteLine();
+
+Show2dArray(
+    SquareArray(
+        CreateRandom2dArray(m, n, -10, 10)));
+
 int[,] CreateRandom2dArray(int m, int n, int minValue, int maxValue) {
     int[,] newArray = new int[m,n];
     for (int i = 0; i < newArray.GetLength(0); i++) {
@@ -19,26 +32,14 @@ void Show2dArray(int[,] array) {
     }
 }
 
-int[,] SquareArray(int[,] array) {
+int[,] SquareArray(int[,] myArray) {
     int[,] newArray = new int[m,n];
-    for (int i = 0; i < array.GetLength(0); i++) {
+    for (int i = 0; i < newArray.GetLength(0); i++) {
         i++;
-        for (int j = 0; j < array.GetLength(1); j++) {
+        for (int j = 0; j < newArray.GetLength(1); j++) {
             j++;
-            array[i,j] = array[i,j] * array[i,j];
+            newArray[i,j] = newArray[i,j] * newArray[i,j];
         }
     }
-    return array;
+    return newArray;
 }
-
-Console.Write("введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите n: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-int[,] myArray = CreateRandom2dArray(newArray, -10, 10);
-Console.WriteLine();
-Show2dArray(myArray);
-Console.WriteLine();
-int[,] finalArray = SquareArray(newArray);
-Show2dArray(finalArray);
