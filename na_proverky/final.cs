d19d80a321be83991c4ffee6bdb25a69eab7e506
Igor <vivpost@mail.ru>
2022-09-27 21:57:39 +0300

    portfolio

---
+++ na_proverky/final.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите данные: ");
+string[] text = Console.ReadLine().Split(new Char[] { ' ' });
+
+StringSize(text);
+void StringSize(string[] text) {
+    for (int i = 0; i < text.Length; i++) {
+        if (text[i].Length <= 3) Console.WriteLine(text[i]);
+    }
+}
