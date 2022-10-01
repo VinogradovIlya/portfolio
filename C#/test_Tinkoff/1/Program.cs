eebb713231c15c12a731a15f87b9186cf88a1768
Igor <vivpost@mail.ru>
2022-10-01 20:59:44 +0300

    portfolio

--- C#/test_Tinkoff/1/Program.cs
+++ C#/test_Tinkoff/1/Program.cs
@@ -3,3 +3,25 @@ Console.WriteLine("пример:");
 Console.WriteLine("6 6 8 8 ");
 Console.WriteLine("1 8 4 9");
 Console.WriteLine("вывод: 49");
+
+string[] input1 = Console.ReadLine().Split(new char[] {' '});
+string[] input2 = Console.ReadLine().Split(new char[] {' '});
+
+int one = Convert.ToInt32(input1[0]);
+int two = Convert.ToInt32(input1[1]);
+int three = Convert.ToInt32(input1[2]);
+int four = Convert.ToInt32(input1[3]);
+int step1 = Math.Abs(one - three);
+int step2 = Math.Abs(two - four);
+int area1 = step1 * step2;
+
+int five = Convert.ToInt32(input2[0]);
+int six = Convert.ToInt32(input2[1]);
+int seven = Convert.ToInt32(input2[2]);
+int eight = Convert.ToInt32(input2[3]);
+int step3 = seven - five;
+int step4 = eight - six;
+int area2 = step3 * step4;
+
+int res = Convert.ToInt32(Math.Pow((area1 + area2), 2));
+Console.WriteLine("{0}", res);
