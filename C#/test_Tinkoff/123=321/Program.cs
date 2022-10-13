ca5eb58a7983914378d56bbd6194eeb7f28cb92e
Igor <vivpost@mail.ru>
2022-10-13 23:59:01 +0300

    portfolio

--- C#/test_Tinkoff/123=321/Program.cs
+++ C#/test_Tinkoff/123=321/Program.cs
@@ -1,6 +1,16 @@
 ﻿Console.Clear();
 Console.WriteLine("1 2 3 == 3 2 1?");
 
+int n = Convert.ToInt32(Console.ReadLine());
+string name = string.Empty;
+string str = string.Empty;
+string[] team = new string[3];
+for (int i = 0; i < n; i++) {
+    name = Console.ReadLine();
+    team[i] = name.ToUpper(); // ставит все буквы в верхний регистр (ToLower в нижний)
+    str = str + team[i] + ' ';
+}
+
 /*
 1 2 3
 ------------
@@ -11,21 +21,25 @@ Console.WriteLine("1 2 3 == 3 2 1?");
 2 3 1
 */
 
-// суть метода: если команда совпадает (какая команда?) с тремя словами из строчки, то max++
-void Proverka(string text, string sub) { 
+int result = 0;
 
-    if (input == str) Console.WriteLine("yes"); // самый простой вариант, где совпадение сразу
-    else {
-        for (int i = 0; i < input.Length; i++) { // идет посимвольная проверка
-            if (input[i] == str[i]) { // пока символы совпадают все хорошо
-                Console.WriteLine("good");
-            } else { // работа с символами, которые не совпадают
-                for (int j = input.Length - i; j < input.Length; j++) { 
-                    // if input[i] == input[j+1];
-                    Console.WriteLine("bad");
-                    break;
-                }
-            }
-        }
+// суть метода: если команда совпадает (какая команда?) с тремя словами из строчки, то max++
+void Proverka(string text, string sub) {
+    string[] allTeams = str.Split(' ');
+    // идет по командам
+    for (int i = 0; i < n; i++) {
+        //вариации
+        string teamVersion1 = allTeams[i+0] + ' ' + allTeams[i+1] + ' ' + allTeams[i+2];
+        string teamVersion2 = allTeams[i+0] + ' ' + allTeams[i+2] + ' ' + allTeams[i+1];
+        string teamVersion3 = allTeams[i+1] + ' ' + allTeams[i+0] + ' ' + allTeams[i+2];
+        string teamVersion4 = allTeams[i+1] + ' ' + allTeams[i+2] + ' ' + allTeams[i+0];
+        string teamVersion5 = allTeams[i+2] + ' ' + allTeams[i+1] + ' ' + allTeams[i+0];
+        string teamVersion6 = allTeams[i+2] + ' ' + allTeams[i+0] + ' ' + allTeams[i+1];
+        if (team[i] == teamVersion1) max++;
+        if (team[i] == teamVersion2) max++;
+        if (team[i] == teamVersion3) max++;
+        if (team[i] == teamVersion4) max++;
+        if (team[i] == teamVersion5) max++;
+        if (team[i] == teamVersion6) max++;
     }
 }
--- C#/test_Tinkoff/2/Program.cs
+++ C#/test_Tinkoff/2/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Clear();
+﻿// Console.Clear();
 Console.WriteLine("Аня - координатор стажировок в Тинькофф. Она хочет нанять самых сильных олимпиадников. Для того чтобы понять, кто же лучший, Аня решила проанализировать результаты командной олимпиады за последние N лет. Она знает все команды, занявшие первое место. Каждая команда задается тройкой имен, причем их порядок не важен, то есть записи ANTON BORIS CHRIS и BORIS ANTON CHRIS задают одну и ту же команду. Ане нужны лучшие из лучших, поэтому она хочет знать, какое максимальное число раз побеждала команда в одном и том же составе. Вы дружите с Аней, поэтому согласились ей помочь.");
 Console.WriteLine("Формат входных данных");
 Console.WriteLine("В первой строке задано одно целое число N (1 ≤ N ≤ 1000) - количество лет, за которые у Ани есть данные. В следующих N строках заданы команды-победители: в каждой строке указаны три разделенных пробелом имени. Каждое имя имеет длину от 1 до 10 символов, а также состоит из заглавных латинских символов ( A, ….., Z).");
@@ -24,68 +24,53 @@ if ((n < 1) || (n > 1000)) {
 
 // чтение данных: сборка в строку + разбивка по командам
     for (int i = 0; i < n; i++) {
-        name = Console.ReadLine();
-        team[i] = name.ToUpper();
-        str = str + name + ' ';
-    }
-    str = str.ToUpper(); // ставит все буквы в верхний регистр (ToLower в нижний)
+    team[i] = Console.ReadLine().ToUpper(); // ставит все буквы в верхний регистр (ToLower в нижний) 
+    str = str + team[i] + ' ';
+}
 
-// основной метод (посмотреть foreach и IndexOf?)
+// основной метод (посмотреть foreach и IndexOf ?)
     int CountRep(string text, string sub ) {
-        int count = 0;
-        int pos = 0;
-        while (true) {
-            pos = text.IndexOf(sub, pos);
-            if (pos != -1) {
-                count++;
-                pos++;
-            } else {
-                break;
-            }
+    int count = 0;
+    int pos = 0;
+    while (true) {
+        pos = text.IndexOf(sub, pos);
+        if (pos != -1) {
+            count++;
+            pos++;
+        } else {
+            break;
         }
-        return count;
-    } 
+    }
+    return count;
+}
+
 // нахождение максимума
     int max = CountRep(str, team[0]);
     for (int i = 0; i < n; i++) {
-        int nextValue = CountRep(str, team[i]);
-        if (nextValue > max) max = nextValue;
-    }
+    int nextValue = CountRep(str, team[i]);
+    if (nextValue > max) max = nextValue;
+}
+
 // калибровка условий
-    /*
-    1 2 3
-    ------------
-    1 3 2
-    3 2 1
-    3 1 2
-    2 1 3
-    2 3 1
-    */
-
-// суть метода: если команда совпадает (какая команда?) с тремя словами из строчки, то max++
-    void Proverka(string text, string sub) { 
-        for (int i = 0; i < text.Length; i+=3) {
-            string new_string = string.Empty;
-            //new_string = text[0] + text[1] + text[2];
-            int result = 0;
-            //if (sub == new_string) result++; // самый простой вариант, где совпадение сразу
-            if (sub == text[0] && text[1] && text[2]) result++;
-            else {
-                for (int i = 0; i < input.Length; i++) { // идет посимвольная проверка
-                    if (input[i] == str[i]) { // пока символы совпадают все хорошо
-                        Console.WriteLine("good");
-                    } else { // работа с символами, которые не совпадают
-                        for (int j = input.Length - i; j < input.Length; j++) { 
-                            // if input[i] == input[j+1];
-                            Console.WriteLine("bad");
-                            break;
-                        }
-                    }
-                }
-            }
+    void Proverka(string text, string sub) {
+        string[] allTeams = str.Split(' ');
+        // идет по командам
+        for (int i = 0; i < n; i++) {
+            //вариации
+            string teamVersion1 = allTeams[i+0] + ' ' + allTeams[i+1] + ' ' + allTeams[i+2];
+            string teamVersion2 = allTeams[i+0] + ' ' + allTeams[i+2] + ' ' + allTeams[i+1];
+            string teamVersion3 = allTeams[i+1] + ' ' + allTeams[i+0] + ' ' + allTeams[i+2];
+            string teamVersion4 = allTeams[i+1] + ' ' + allTeams[i+2] + ' ' + allTeams[i+0];
+            string teamVersion5 = allTeams[i+2] + ' ' + allTeams[i+1] + ' ' + allTeams[i+0];
+            string teamVersion6 = allTeams[i+2] + ' ' + allTeams[i+0] + ' ' + allTeams[i+1];
+            if (team[i] == teamVersion1) max++;
+            if (team[i] == teamVersion2) max++;
+            if (team[i] == teamVersion3) max++;
+            if (team[i] == teamVersion4) max++;
+            if (team[i] == teamVersion5) max++;
+            if (team[i] == teamVersion6) max++;
         }
-            
     }
-
+    Proverka(str, team[0]);
     Console.WriteLine("{0}", max); // Console.WriteLine($"{max}"); // Console.WriteLine(max + " ");
 }
