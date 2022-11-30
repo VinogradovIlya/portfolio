d3aa6da083513bd9c90830a3fa01433b538fb6ec
Igor <vivpost@mail.ru>
2022-12-01 00:18:55 +0300

    portfolio

--- C#/test_Tinkoff/5/Program.cs
+++ C#/test_Tinkoff/5/Program.cs
@@ -24,9 +24,54 @@ for (int i = 0; i < q; i++) {
 	nameZapros.Add(inputQ[1]);
 }
 
- List<int> result = new List<int>(q);
+List<string> surnameList = new List<string>(q);
+void Sorted(List<string> inputNSortCopy, List<string> nameZapros, int i) {
+	foreach (string surname in inputNSortCopy) { // прохожу по фамилиям из отсортированного массива
+		if (Regex.IsMatch(surname, nameZapros[i], RegexOptions.IgnoreCase)) { // нахожу однокоренные фамилии
+			surnameList.Add(surname); // добавляю подходящие элементы
+		}
+	}
+	
+	System.Console.WriteLine("---------");
+	foreach (var item in surnameList)
+	{
+		System.Console.WriteLine(item);
+	}
+}
 
-Sort(inputN, inputNSortCopy, numberZapros, nameZapros);
+void Delete(List<string> surnameList, int i) {
+	for (int j = 1; j < numberZapros[i]; j++) { //удаляю лишние предыдущие фамилии
+		surnameList.RemoveAt(0);
+	}
+}
+
+List<int> result = new List<int>();
+// определяю номера искомых фамилий
+void Result(List<string> surnameList, List<int> result, int i) {
+	int chislo = inputN.IndexOf(surnameList[0]);
+	chislo++;
+	result.Add(chislo);
+}
+
+for (int i = 0; i < q; i++) {
+	Sorted(inputNSortCopy, nameZapros, i);
+	Delete(surnameList, i);
+	Result(surnameList, result, i);
+	surnameList.Clear();
+}
+
+//ответ
+foreach (var item in result)
+{
+	Console.WriteLine(item);
+}
+
+/*********
+bool Contains(T item): возвращает true, если элемент item есть в списке
+
+int IndexOf(T item): возвращает индекс первого вхождения элемента в списке
+
+алгоритм метода:
 // --------------------
 // 1. приходит отсортированный список
 // (запрос 2 ab (в списке abc, aboba, ad, a, b))
@@ -34,58 +79,5 @@ Sort(inputN, inputNSortCopy, numberZapros, nameZapros);
 // 3. из нового отсеянного списка выбирается фамилия соответсвующая номеру в запросе (цифра 2 => aboba)
 // 4. в исходном списке ищется указанная фамилия (aboba) и возвращается номер указанной фамилии
 // --------------------
-void Sort(List<string> inputN, List<string> inputNSortCopy, 
-	List<int> numberZapros, List<string> nameZapros) {
-	
-	List<string> surnameList = new List<string>(q);
-	int count = nameZapros.Count; // == q
-	for (int i = 0; i < count; i++) { // прохожу по каждому запросу
-		foreach (string surname in inputNSortCopy) { // прохожу по фамилиям из отсортированного массива
-			if (Regex.IsMatch(surname, nameZapros[i], RegexOptions.IgnoreCase)) { // нахожу однокоренные фамилии
-				surnameList.Add(surname); // добавляю подходящие элементы
-			}
-		}
 
-		for (int j = 1; j < numberZapros[i]; j++) { //удаляю лишние предыдущие фамилии
-			surnameList.RemoveAt(0);
-		}
-	}
-
-	Console.WriteLine("------"); // test
-	foreach (var item in surnameList)
-	{
-		Console.WriteLine(item);
-	}
-
-}
-
-
-/*********
-bool Contains(T item): возвращает true, если элемент item есть в списке
-
-int IndexOf(T item): возвращает индекс первого вхождения элемента в списке
-				//int vhod = inputNSortCopy.IndexOf(nameZapros[number]);
-				// inputNSortCopy.Remove(inputNSortCopy[vhod]);
-
-алгоритм метода:
-что входит: фамилии (+ место для хранения отсортированного списка) + условия
-что выходит номер фамилии в неотсортированном списке
-
-как работает метод
-
-указывается первое вхождение фамилии по указанным условиям в отсортированном списке
-сохраняется позиция
-
-string[] rows = {"Москва— столица Российской Федерации.",
-                         "Это предложение оказалось здесь случайно",
-                         "Москва - город федерального значения, административный центр Центрального федерального округа и центр Московской области.",
-                         "Жители столицы - москвичи.",
-                         "Как и это."};
-        foreach (string row in rows)
-        {
-            if (Regex.IsMatch(row, "моск", RegexOptions.IgnoreCase)) Указывает, обнаружено ли в указанной входной строке соответствие заданному регулярному выражению, используя указанные параметры сопоставления.
-                Console.WriteLine(row);
-            foreach (Match match in Regex.Matches(row, @"\b\w*моск\w*\b", RegexOptions.IgnoreCase))
-                Console.WriteLine("\t\t{0}", match.Value);
-        }
 */
