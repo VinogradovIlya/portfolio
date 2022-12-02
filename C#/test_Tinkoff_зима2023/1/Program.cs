71218e7407e78f996ad0b44c6830e2d27a948173
Igor <vivpost@mail.ru>
2022-12-02 17:33:01 +0300

    portfolio

---
+++ C#/test_Tinkoff_зима2023/1/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+string[] s = Console.ReadLine().Split();
+string b = Console.ReadLine();
+
+int count = 0; // счетчик слов
+
+for (int i = 0; i < s.Length; i++) { //прохожу по словам
+    int lengthWord = s[i].Length;// длина слова
+    char[] symbols = b.ToCharArray(); // заданная поклеточная раскраска
+
+    int j = lengthWord * i; // поправочный коэффициент;
+    while (j < n-1) { // прохожу посимвольно по одному слову
+        j++;
+        if (symbols[j] == symbols[j-1]) {
+            count++;
+            break;
+        }
+    }
+    
+}
+Console.WriteLine($"{count}");
