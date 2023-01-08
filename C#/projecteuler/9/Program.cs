c26f4607adc3e04c02d8253eaa2882ec2a41e26f
Igor <vivpost@mail.ru>
2023-01-08 19:53:31 +0300

    portfolio

--- C#/projecteuler/9/Program.cs
+++ C#/projecteuler/9/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 
 /**
 https://projecteuler.net/problem=35
@@ -17,6 +18,36 @@ class Program
 {
     static void Main()
     {
+        int task = 1_00;//0_000;
 
+        List<int> circles = new List<int>();
+        void NewTurn(int number)  // 197 -> 7*100 + 19 == 719 -> 9*100 + 71 == 971 -> 1*100 + 97 == 197
+        {
+            int count = number;
+            while (count != 0)
+            {
+            int temp = number % 10; 
+            temp *= 100;
+            number /= 10;
+            number += temp;
+            circles.Add(number);
+            count /= 10;
+            }
+        }
+
+        bool PrimeNumber(int number)
+        {
+            if (number == 2 || number == 3) return true;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+        for (int i = 2; i < task; i++)
+        {
+            if (PrimeNumber(i)) NewTurn(i);
+        }
     }
 }
