83adda0bc1ba2600d5af8eabe01c8d484c886ee9
vnovigr <vivpost@mail.ru>
2023-03-13 23:05:19 +0300

    gg

---
+++ Java/YlabUniversity/2/Sequences/Sequences.java
@@ -0,0 +1,22 @@
+public interface Sequences {
+
+    void a(int n);
+
+    void b(int n);
+
+    void c(int n);
+
+    void d(int n);
+
+    void e(int n);
+
+    void f(int n);
+
+    void g(int n);
+
+    void h(int n);
+
+    void i(int n);
+
+    void j(int n);
+}
