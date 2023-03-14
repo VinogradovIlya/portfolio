f20658af364e71041d7c1000bf8028ab122651c6
vnovigr <vivpost@mail.ru>
2023-03-14 13:07:24 +0300

    gg

--- Java/YlabUniversity/2/RateLimitedPrinter/Main.java
+++ Java/YlabUniversity/2/RateLimitedPrinter/Main.java
@@ -7,20 +7,8 @@ public class Main {
 
         RateLimitedPtinter rlp = new RateLimitedPtinter(1000);
 
-        long timeStart = System.currentTimeMillis();
-        long timePrint = timeStart + rlp.getInterval();
-
         for (int i = 0; i < 1_000_000_000; i++) {
-            long timeNow = System.currentTimeMillis();
-
-            if (timeNow >= timePrint) {
-                rlp.print(String.valueOf(i));
-                timeStart = System.currentTimeMillis();
-                // Thread.sleep(rlp.getInterval());
-            }
-
-                Thread.sleep(rlp.getInterval());
+            rlp.print(String.valueOf(i));
         }
-        System.out.println(System.currentTimeMillis());
     }
 }
--- Java/YlabUniversity/2/RateLimitedPrinter/RateLimitedPtinter.java
+++ Java/YlabUniversity/2/RateLimitedPrinter/RateLimitedPtinter.java
@@ -4,13 +4,18 @@
 public class RateLimitedPtinter {
 
     private int interval;
+    private long lastCallTime = 0;
 
     public RateLimitedPtinter(int interval) {
         this.interval = interval;
     }
-    
+
     public void print(String message) {
-        System.out.println(message);
+        long startTime = System.currentTimeMillis();
+        if (startTime - lastCallTime > getInterval()) {
+            lastCallTime = startTime;
+            System.out.println(message);
+        }
     }
 
     public int getInterval() {
