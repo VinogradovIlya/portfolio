5cefd77ee85caa0fae47c28166a867f31c5db809
IlyaVinigradov <vivpost@mail.ru>
2023-12-08 12:40:41 +0300

    appearance module implemented

--- python/time_management/view.py
+++ python/time_management/view.py
@@ -10,9 +10,7 @@ class View(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        # главный экран виджет, для удобного прехода между окнами
         self.main_win = QStackedWidget()
-        # сделать main_win главным экраном
         self.setCentralWidget(self.main_win)
         self.set_appear()
         self.main_win_ui()
@@ -20,11 +18,8 @@ class View(QMainWindow):
 
     def set_appear(self):
         """ метод для формирования окна """
-        # создание названия окна
         self.setWindowTitle('My time')
-        # цвет заднего фона
         self.setStyleSheet("background-color: rgb(207, 208, 118)")
-        # один метод для размеров экрана и появления в координатах
         self.setGeometry(200, 129, 500, 300)
 
     def main_win_ui(self):
@@ -32,20 +27,18 @@ class View(QMainWindow):
         2 кнопки: добавить/удалить/обновить и посмотреть результат
         """
         self.btn_add = QPushButton('Добавить часы')
-        # переход к новому экрану
         self.btn_add.clicked.connect(self.add_screen_ui)
         self.btn_show = QPushButton('Показать отработанные часы')
 
-        # создание лэйаута (направляющей линии, где будут наши виджеты (кнопки, текст и тд))
         self.layout_widget = QWidget()
         self.v_layout = QVBoxLayout()
-        # добавление виджетов на направляющие линии и расположение
+
         self.v_layout.addWidget(
             self.btn_add, alignment=Qt.AlignmentFlag.AlignCenter)
         self.v_layout.addWidget(
             self.btn_show, alignment=Qt.AlignmentFlag.AlignCenter)
         self.layout_widget.setLayout(self.v_layout)
-        # добавление виджета на экран
+
         self.main_win.addWidget(self.layout_widget)
 
     def add_screen_ui(self):
@@ -58,7 +51,7 @@ class View(QMainWindow):
         # self.my_time.setMinimum(0)
         # выбор дня
         self.day = QLabel('Выберите день')
-        # календарь
+
         self.cal = QCalendarWidget()
         self.cal.setGridVisible(True)
         self.cal.resize(310, 200)
@@ -74,17 +67,17 @@ class View(QMainWindow):
         self.surname_lbl = QLabel(
             'Введите фамилии учеников (время можно указать в скобках) через ";"')
         self.surname = QLineEdit()
-        # кнопка для сохранения
+
         self.btn_save = QPushButton('Сохранить')
         self.btn_save.clicked.connect(Model.done)
-        # кнопка назад
+
         self.btn_back = QPushButton('Назад')
         self.btn_back.clicked.connect(self.open_main_win)
 
-        # лэйаут для расположения виджетов на экране
         self.layout_widget = QWidget()
         self.c_line = QVBoxLayout()
         self.bottom_h_line = QHBoxLayout()
+
         self.bottom_h_line.addWidget(
             self.btn_back, alignment=Qt.AlignmentFlag.AlignCenter)
         self.bottom_h_line.addWidget(
@@ -102,6 +95,7 @@ class View(QMainWindow):
             self.surname_lbl, alignment=Qt.AlignmentFlag.AlignCenter)
         self.c_line.addWidget(
             self.surname, alignment=Qt.AlignmentFlag.AlignCenter)
+
         self.c_line.addLayout(self.bottom_h_line)
         self.layout_widget.setLayout(self.c_line)
         self.main_win.addWidget(self.layout_widget)
