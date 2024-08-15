885b377b4aa9a603a4d094712f6a97096d0c9b82
Игорь Воинов <igorvoinov@MacBook-Air-Igor.local>
2024-08-15 18:55:37 +0300

    obj.__getattribute__() not working

---
+++ python/банкомат (ООП)/main.py
@@ -0,0 +1,14 @@
+from new_ATM import Atm
+from decimal import Decimal
+
+
+atm = Atm(10_000_000)
+while move := input('Введите действие:\n1 - пополнить\n2 - снять\n3 - выйти\n'):
+    if move == '1':
+        money = Decimal(input('Введите сумму для пополнения: '))
+        if atm.check50(money):
+            atm.add_rub(money)
+    elif move == '3':
+        break
+    atm.check_wealth()
+    print(atm)
--- python/банкомат (ООП)/new_ATM.py
+++ python/банкомат (ООП)/new_ATM.py
@@ -3,17 +3,42 @@ from typing import Any
 
 
 class Atm:
-    def __init__(self, rub: int | float | Decimal = 0) -> None:
+    def __init__(self, rub: int | float = 0) -> None:
         """ Метод инициализации экземпляра """
-        self._rub = rub
+        self._rub = Decimal(rub)
+        self._count = 0
 
-    def add_rub(self, money: int | float | Decimal = 0) -> None:
+    def add_rub(self, money: int | float) -> None:
         """ Метод для работы с начислением рублей экземпляра """
-        self._rub += money
+        money = Decimal(money)
+        if money > 0:
+            self += money
+        else:
+            raise ValueError('Некорректное значение')
 
     def take_rub(self, money: int | float | Decimal = 0) -> None:
         """ Метод для работы со списанием рублей экземпляра """
-        self._rub -= money
+        if money > self._rub:
+            raise ValueError('Сумма больше, чем денег на счете')
+        percent = money * 1.015
+        if not 30 < percent < 600:
+            if percent < 30:
+                percent = 30
+            elif percent > 600:
+                percent = 600
+        self -= percent  # доделать in place вычитание
+
+    def check50(self, money: int | float | Decimal = 0) -> None:
+        """ Метод для проверки кратности 50 """
+        money = Decimal(money)
+        if money % 50:
+            raise ValueError('Значение должно быть кратным 50')
+        return not money % 50
+
+    def check_wealth(self):
+        """ Метод для списания налога на богатство """
+        if self._rub > 5_000_000:
+            self._rub *= 0.9
 
     def __str__(self) -> str:
         """ Метод для строчного представления экземпляра """
@@ -23,49 +48,51 @@ class Atm:
         """ Метод для представления экземпляра для программиста"""
         return f'Atm({self._rub})'
 
-    def __iadd__(self, money: int | float | Decimal = 0) -> Any:
+    def __iadd__(self, money: int | float | Decimal) -> Any:
         """ Метод для сложения экземпляра и числа \n(вспомогательный метод для начисления рублей) """
         self._rub = self._rub + money
         return Atm(self._rub)
+
+    # def __getattribute__(self, currency: str) -> Any:
+    #     """ Метод для работы с попыткой обращения к атрибутам экземпляра """
+    #     if not currency in ('_rub', '_count'):
+    #         raise AttributeError(
+    #             'Error 1: Ведутся технические работы, пока что возможна только работа в рублях')
+    #     return object.__getattribute__(self, currency)
+
+    def __setattr__(self, name, value) -> None:
+        """ Метод для попытки присвоения значения атрибуту экземпляра """
+        if not name in ('_rub', '_count'):
+            raise AttributeError(
+                'Error 2: Ведутся технические работы, пока что возможна только работа в рублях')
+        return object.__setattr__(self, name, value)
 
     def __getattr__(self, item) -> None:
         """ Метод для попытки присвоения значения несуществующему атрибуту экземпляра """
         return None
 
-    def __getattribute__(self, currency: str) -> Any:
-        """ Метод для работы с попыткой обращения к атрибутам экземпляра """
-        if currency != '_rub':
-            raise AttributeError(
-            'Ведутся технические работы, пока что возможна только работа в рублях')
-        return object.__getattribute__(self, currency)
-
-    def __setattr__(self, name, value) -> None:
-        """ Метод для попытки присвоения значения атрибуту экземпляра """
-        if name != '_rub':
-            raise AttributeError(
-                'Ведутся технические работы, пока что возможна только работа в рублях')
-        return object.__setattr__(self, name, value)
-
     def __delattr__(self, item):
-        if item in ('_rub'):
+        if item in ('_rub', '_count'):
             setattr(self, item, 0)
         else:
             object.__delattr__(self, item)
 
 
 if __name__ == '__main__':
-    help(Atm)
+    # help(Atm)
     atm = Atm()
     print(atm)
 
-    atm += 50
+    atm.add_rub(50)
     print(atm)
 
-    # atm._usd = 100  # AttributeError: Ведутся технические работы, пока что возможна только работа в рублях
+    # atm._usd = 100  # AttributeError: Error 2: Ведутся технические работы, пока что возможна только работа в рублях
     print(atm._usd)  # None
 
     atm._rub = 100
     print(atm)
+
+    print(atm.check50(100))
 
     del atm._rub
     print(atm)
